797f32bb24759167c0c9bf8d25b00c558ead4567
paayi <50712424+paayi@users.noreply.github.com>
2020-03-13 09:34:36 -0400

    Update index.js

--- function0/index.js
+++ function0/index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello world 2nd attempt!'),
+        body: JSON.stringify('Major Change!'),
     };
     return response;
 };
